d3372b79e415b45907ec4bdf41e7897986529d62
yossi-lerer <95751422+yossi-lerer@users.noreply.github.com>
2024-07-30 00:06:05 +0300

    v1

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
+const host = process.env.HOST
 
 app.use(cookieParser());
 app.use(express.json());
@@ -68,6 +69,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
 });
